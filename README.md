f9dd9e1e685a88632baed907ad865a74d7299309
Benjamin Gysin <benjamin.gysin@gmail.com>
2014-02-14 17:19:33 +0100

    inital checkin

---
+++ README.md
@@ -0,0 +1,10 @@
+This sketch measures the current up to 26V / 3.2A of an electrical consumer.
+It uses the INA219 High Side DC Current Sensor Breakout Board from Adafruit.
+
+Requirements
+* Breakout Board: http://www.adafruit.com/products/904
+* Adafruit Library: http://learn.adafruit.com/adafruit-ina219-current-sensor-breakout/downloads
+
+Tutorial can be found here: http://learn.adafruit.com/adafruit-ina219-current-sensor-breakout/overview
+
+The sketch puts out shunt voltage, load voltage and current with a 10 seconds interval.
---
+++ arduino_current_logger.ino
@@ -0,0 +1,35 @@
+#include <Wire.h>
+#include <Adafruit_INA219.h>
+
+// http://www.adafruit.com/products/904
+// http://learn.adafruit.com/adafruit-ina219-current-sensor-breakout/overview
+
+Adafruit_INA219 ina219;
+
+void setup(void) 
+{
+  uint32_t currentFrequency;
+    
+  Serial.begin(115200);
+  ina219.begin();
+}
+
+void loop(void) 
+{
+  float shuntvoltage = 0;
+  float busvoltage = 0;
+  float current_mA = 0;
+  float loadvoltage = 0;
+
+  shuntvoltage = ina219.getShuntVoltage_mV();
+  //busvoltage = ina219.getBusVoltage_V();
+  current_mA = ina219.getCurrent_mA();
+  loadvoltage = busvoltage + (shuntvoltage / 1000);
+  
+  //Serial.print("Bus Voltage:   "); Serial.print(busvoltage); Serial.println(" V");
+  Serial.print("Shunt: "); Serial.print(shuntvoltage); Serial.print("mV, ");
+  Serial.print("Load: "); Serial.print(loadvoltage); Serial.print("V, ");
+  Serial.print("Current: "); Serial.print(current_mA); Serial.print("mA");
+  Serial.println("");
+  delay(10000);
+}
